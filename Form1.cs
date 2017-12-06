06c79406ef913749d3cbe36c0d3beb0fb15c6571
kabeer1639 <33493671+kabeer1639@users.noreply.github.com>
2017-12-06 15:15:21 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = this.Controls["nameTextBox"].Text;
+            string doj = this.Controls["dojPicker"].Text;
+            string city = this.Controls["cityPicker"].Text;
+            string skills = this.Controls["skillsProvider"].Text;
+            string experience=this.Controls["experienceInput"].Text;
+            string mobileno = this.Controls["moileInput"].Text;
+            string language = this.Controls["languageGroup"].Text;
+
+
+        }
+    }
+}
